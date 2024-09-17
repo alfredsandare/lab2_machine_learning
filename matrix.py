05364303eb0786857d5a36403ba002e892df0ca4
Sommartomten <jonatan.nystrom@willandsfarm.se>
2024-09-17 16:15:31 +0200

    matmul function added

--- matrix.py
+++ matrix.py
@@ -1,2 +1,28 @@
 def transpose(matrix):
     return [[row[j] for row in matrix] for j in range(len(matrix[0]))]
+
+
+def matmul(matrixa, matrixb):
+    output=[]
+    if len(matrixa[0])== len(matrixb):
+        for n in range(len(matrixa)):
+            output.append([])
+            for i in range(len(matrixb[0])):
+                output[n].append(0)
+
+        for row_id,alist in enumerate(matrixa):
+            for i,itema in enumerate(alist):
+                for column_id, itemb in enumerate(matrixb[i]):
+                    output[row_id][column_id]+=itema*itemb
+    else:
+        for n in range(len(matrixb)):
+            output.append([])
+            for i in range(len(matrixa[0])):
+                output[n].append(0)
+
+        for row_id,blist in enumerate(matrixb):
+            for i,itemb in enumerate(blist):
+                for column_id, itema in enumerate(matrixa[i]):
+                    output[row_id][column_id]+=itema*itemb
+                      
+    return(output)
