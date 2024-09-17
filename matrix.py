978b51376ebd061cba7ad195cc8dc82811b2ea5a
Alfred <alfred.s.sandare04@gmail.com>
2024-09-17 20:01:16 +0200

    added powers() functions

--- matrix.py
+++ matrix.py
@@ -1,2 +1,5 @@
 def transpose(matrix):
     return [[row[j] for row in matrix] for j in range(len(matrix[0]))]
+
+def powers(input_list, lower_limit, upper_limit):
+    return [[i**j for j in range(lower_limit, upper_limit+1)] for i in input_list]
