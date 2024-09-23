58431521eec7a9b6fb32aa839ff302b4a63f88f8
Sommartomten <jonatan.nystrom@willandsfarm.se>
2024-09-23 10:42:47 +0200

    added native_regression

---
+++ native_regression.py
@@ -0,0 +1,17 @@
+from matrix import *
+import sys
+import matplotlib.pyplot as plt
+path = sys.argv[1]
+x,y=transpose(loadtxt(path))
+Xp= powers(x,0,1)
+Yp= powers(y,1,1)
+Xpt = transpose(Xp)
+[[b],[m]]=matmul(invert(matmul(Xpt,Xp)),matmul(Xpt,Yp))
+print(b)
+print(m)
+y2=[]
+for temperature in x:
+    y2.append((b + m * temperature))
+plt.plot(x,y,'ro')
+plt.plot(x,y2)
+plt.show()
